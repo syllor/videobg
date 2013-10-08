0c2a7bd60628e737034d0b32c05f801605d275a5
syllor <syllormore@gmail.com>
2013-10-09 01:30:15 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ videobg
 =======
 
 use html5 video as background 
+
+this one works the same as spotify
+
+https://www.spotify.com/us/video-splash/
+
++ do not works on cell phone
+
++ ie7 ie8 and other bowser which can not support video will repeat show images
+
++ chrome firefox safari ie9+ the video works
+
+
+##setup
+this is depend on jquery 
+var yourvideobg = new videobg("#main",options)
+
+
+##options
+
+#####option.width 
+#####option.height
+you have to provide a video width height,then the script need init with the video ratio
+
+#####option.webm
+#####option.mp4
+#####option.ogg
+need 3 types of the video
+
+#####options.imgs
+this is a set of images or one image
+when the array length is not 1,a set of images will repeat shown in 10sec
+
+if length is 1,there will be a image as background
---
+++ videobg.js
@@ -0,0 +1,162 @@
+
+videobg = function(el,options){
+  this.options = options
+  this.ele = $(el);
+  this.ratio = this.options.height/this.options.width;
+  this.videobg = $("<div/>").css({
+    position:'fixed',
+    overflow:'hidden',
+    top:0,
+    left:0,
+    width:'100%',
+    height:'100%',
+    'z-index':0
+  })
+  this.video= $("<video/>").css({
+    position:'absolute',
+    top:'50%',
+    left:0,
+    'z-index':0
+  }).attr({
+    autoplay:true,
+    loop:true,
+    preload:'auto'
+  }),
+  this.wrapper = $("<div/>").addClass('video_wrapper')
+      .css({
+        position:'absolute',
+        top:0,
+        left:0,
+        'z-index':1,
+        width:'100%',
+        height:'100%'
+      })
+}
+videobg.prototype = {
+  init:function(){
+    var that = this;
+    if (this.isMobile())
+      return;
+
+    if (this.canplay()) {
+      if (this.supportType('webm')){
+        this.video.attr('src',this.options.webm);
+      }
+      else if (this.supportType('mp4')) {     
+        this.video.attr('src',this.options.mp4);
+      }
+      else {
+        this.video.attr('src',this.options.ogv);
+      }
+      this.fixheight();
+      window.onresize = function(){
+        that.fixheight();
+      }
+    }else{
+      this.buildimgslide();
+    }
+    this.ele.wrap(this.wrapper);
+    this.videobg.addClass('videobg')
+    this.videobg.append(this.video);
+    $("body").append(this.videobg);
+   
+  },
+  buildimgslide:function(){
+    var that = this;
+    // load all images when page load then bind event
+    var len = this.options.imgs.length;
+    if(len == 1){
+      var img = $('<img/>');
+      img.attr('src',this.options.imgs[0])
+          .css('position','absolute')
+          .css('top',0)
+          .css('left',0)
+          .css('min-width','100%')
+          .css('min-height','100%')
+      return this.videobg.append(img);
+    }
+    var index = 0;
+    $(this.options.imgs).each(function(i,item){
+      var img = $('<img/>');
+      var url = that.options.imgs[i];
+      img.attr('src',url)
+          .css('position','absolute')
+          .css('top',0)
+          .css('left',0)
+          .css('min-width','100%')
+          .css('min-height','100%')
+          .css('opacity','0');
+        that.videobg.append(img);
+    })
+    function changeimg(){
+      $(".videobg img").eq(index-1).animate({
+        opacity:0
+      },1000);
+      $(".videobg img").eq(index).animate({
+        opacity:1
+      },1000,function(){
+        if (index == len-1){
+          index = 0;
+        }else{
+          index += 1;
+        }
+        setTimeout(function(){
+          changeimg();
+        },10000)
+      })
+    }
+
+    $(document).ready(function() {
+      changeimg();    
+    })
+  },
+  fixheight:function(){
+    var h = $(window).height();
+    var w = $(window).width();
+    //console.log(h)
+    //console.log(w)
+    //console.log(h/w);
+    //console.log(this.ratio)
+    if(h/w < this.ratio ){
+      this.video.css('width',w);
+      this.video.css('height','auto')
+      this.video.css('margin-top',-w*this.ratio/2)
+      this.video.css('margin-left','0px')
+      this.video.css('top','50%')
+      this.video.css('left','0')
+    }else{
+      this.video.css('height',h)
+      this.video.css('width','auto');
+      this.video.css('margin-left',-h/this.ratio/2)
+      this.video.css('margin-top','0px')
+      this.video.css('top','0')
+      this.video.css('left','50%')
+    }
+  },
+  canplay:function(){
+    var can = document.createElement('video');
+    if(can.canPlayType){
+      return true;
+    } else{
+      return false;
+    }
+  },
+  isMobile : function (){
+    return (navigator.userAgent.match( /iphone|ipad|ipod|android|blackberry|mini|windowssce|palm/) );
+  },
+  supportType:function(str){
+    var v = document.createElement('video');
+    switch (str) {
+      case 'webm' :
+        return (v.canPlayType('video/webm; codecs="vp8, vorbis"'));
+        break;
+      case 'mp4' :
+        return (v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"'));
+        break;
+      case 'ogv' :
+        return (v.canPlayType('video/ogg; codecs="theora, vorbis"'));
+        break;      
+    }
+  }
+}
+
